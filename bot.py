fa6506edf3b74e460eef932c671f1fae005e7e2d
landanfp <159138257+landanfp@users.noreply.github.com>
2025-08-31 14:59:22 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '1806450812:AAGhHSWPd3sH5SVFBB8_Xadw_SbdbvZm0_Q'
+BOT_TOKEN = '5088657122:AAG0WWBVTcR_pg8Lxb719nyfUqaGq1IZdk4'
 #LOG_CHANNEL = -1001792962793  # مقدار دلخواه
 
 app = Client("okru_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
